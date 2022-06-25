062e9ec38c13751f3d1b0a1c902aa31fa3ec57d6
Agung Kurniawan <agung.kurniawan1894@gmail.com>
2022-06-26 00:01:52 +0700

    Challenge 09 Agung Kurniawan (152235865101-374)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import './Header.css'
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <header>
+    <h1>Call a Friend</h1>
+    <p>Your friendly contact app</p>
+    <hr/>
+</header>
+  );
+};
 
 export default Header;
